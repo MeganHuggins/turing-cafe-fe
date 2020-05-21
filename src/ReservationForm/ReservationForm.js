552cd16d2d771e597708fe6faeeba711103f9986
Megan Huggins <Muggins1265@gmail.com>
2020-05-21 10:51:56 -0600

    Set up form functionality to update forms state with new reservations

--- src/ReservationForm/ReservationForm.js
+++ src/ReservationForm/ReservationForm.js
@@ -12,14 +12,35 @@ class ReservationForm extends Component {
     }
   }
 
+  updateReservation = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  bookReservation = (e) => {
+    e.preventDefault();
+    const newReservation = {
+      ...this.state
+    }
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: '',
+      date: '',
+      time: '',
+      number: ''
+    })
+  }
+
   render () {
     return (
       <form classsName='res-form'>
-        <input type='text' placeholder='Name' value={this.state.name} />
-        <input type='text' placeholder='date' value={this.state.date} />
-        <input type='text' placeholder='time' value={this.state.time} />
-        <input type='number' placeholder='number' value={this.state.number} />
-        <button>Make Reservation</button>
+        <input type='text' placeholder='Name' name='name' value={this.state.name} onChange={this.updateReservation} />
+        <input type='text' placeholder='date' name='date' value={this.state.date} onChange={this.updateReservation} />
+        <input type='text' placeholder='time' name='time' value={this.state.time} onChange={this.updateReservation} />
+        <input type='number' placeholder='number' name='number' value={this.state.number} onChange={this.updateReservation} />
+        <button onClick={this.bookReservation}>Make Reservation</button>
       </form>
     )
   }
